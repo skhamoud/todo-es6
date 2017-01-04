ab13ae4ca4b496ec63c68ee7fa0020feff627974
skhamoud <skhamoud@gmail.com>
2017-01-04 18:32:13 +0000

    return idex.html to rootfolder

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+"use strict";
 var gulp = require('gulp');
 var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
@@ -10,12 +11,12 @@ var autoprefixer= require('gulp-autoprefixer');
 // Files 
 //=====================================
 
-let basePath = './';
+let basePath = './public/';
 let src = {
-  sass : basePath + 'sass/' ,
+  sass : 'sass/' ,
   js : basePath + "js/"
 };
-let public = basePath + 'public/';
+
 
 // ====================================
 //  tasks
@@ -26,24 +27,24 @@ gulp.task("styles", function () {
   return gulp.src(src.sass + "main.+(sass|scss)")
 .pipe(sass().on('error', sass.logError))
 .pipe(autoprefixer())
-.pipe(gulp.dest(public))
+.pipe(gulp.dest(basePath))
 .pipe(browserSync.reload({stream:true}));
 });
 
 // Serve files from base directory
-gulp.task('serve', ['styles'], function(){
+gulp.task('serve', ['styles'], function () {
   browserSync.init({
     server: {
-      baseDir: public
+      baseDir: "./"
     }
   });
-})
+});
 
 
 // watches files and manually reloads after html files change
-gulp.task('watch' ,()=> {
-    gulp.watch(`${src.sass}*` , ['styles']);
-    gulp.watch(public + "*.js").on('change', reload);
-    gulp.watch('./**/*.html').on('change', reload);
-})
+gulp.task('watch', () => {
+  gulp.watch(`${src.sass}*`, ['styles']);
+  gulp.watch(basePath + "*.js").on('change', reload);
+  gulp.watch('./*.html').on('change', reload);
+});
 gulp.task("default", ["watch",'serve']);
